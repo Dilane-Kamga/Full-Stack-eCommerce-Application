94809da3030d341b2522c0862954aea1b548a982
Dilane-Kamga <dilanekamga777@hotmail.com>
2023-03-28 04:53:58 +0100

    backend application progress to checkout

---
+++ backend/src/main/java/com/teamsolutions/ecommerceapplication/dao/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.teamsolutions.ecommerceapplication.dao;
+
+
+import com.teamsolutions.ecommerceapplication.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String theEmail);
+}
---
+++ backend/src/main/java/com/teamsolutions/ecommerceapplication/dto/PurchaseResponse.java
@@ -0,0 +1,10 @@
+package com.teamsolutions.ecommerceapplication.dto;
+
+import lombok.Data;
+
+@Data
+public class PurchaseResponse {
+
+    private final String orderTrackingNumber;
+
+}
